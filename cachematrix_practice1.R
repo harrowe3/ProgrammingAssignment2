ba74b1a9b9438d94e1e27ec28ea74e08b835a1c8
Harvey Rowe <harrowe3@gmail.com>
2016-01-20 17:45:11 -0500

    Committing practice file

---
+++ cachematrix_practice1.R
@@ -0,0 +1,19 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## These functions can potentially save the computation cost of
+## inverting a matrix repeatedly, by caching the inverse.
+
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
